ca4c1d4eff436ab892e717d455c4238de0e27f60
jeremydmoore <Jeremy@MooreCreativeArts.com>
2022-12-01 12:41:23 -0500

    add boilerplate and includes

---
+++ 6_readWriteImages/6_readWriteImages.cpp
@@ -0,0 +1,16 @@
+/* 
+    projects/learnCPP/6_readWriteImages.cpp
+
+    Expected behavior:
+    1. create an image with openCV
+    2. save image created
+    3. open image from disk
+
+    Requirements:
+    1. save image using user input filename
+
+    Jeremy@MooreCreativeArts.com
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
